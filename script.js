991d4971f36e6bcad21e8e5c0367f3281b363117
Imam ariadi <121498231+ImamAriadi2022@users.noreply.github.com>
2024-03-18 09:34:37 +0700

    imam commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical', // Menentukan arah slide menjadi vertikal
+    slidesPerView: 'auto', // Mengatur jumlah slide yang terlihat dalam satu waktu
+    spaceBetween: 30, // Mengatur jarak antara slide
+    mousewheel: true, // Memungkinkan navigasi slide menggunakan scroll mouse
+  });
+  
