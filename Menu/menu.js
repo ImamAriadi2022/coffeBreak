64397ef090c0dca143843e70c193106c83b610a9
Imam ariadi <121498231+ImamAriadi2022@users.noreply.github.com>
2024-03-24 16:48:37 +0700

    card 3 fitur

--- Menu/menu.js
+++ Menu/menu.js
@@ -90,13 +90,70 @@ document.getElementById("orderFormContent2").addEventListener("submit", function
 });
 
 
-//link to whats app
+//link to whats app cars 2
 function orderNow2 (){
   var coffeeNameKedua = document.getElementById("coffeeName2").value;
   var priceKedua = document.getElementById("price2").value;
   var quantityKedua = document.getElementById("quantity2").value;
 
   var message = "Pesanan Anda : \n" + coffeeNameKedua + "\nHarga satuan : " + priceKedua + "\nHarga Total : " + (priceKedua * quantityKedua);
+
+  var adminPhoneNumber = "6285788322061";
+  var whatsappURL = "https://api.whatsapp.com/send?phone=" + adminPhoneNumber + "&text=" + encodeURIComponent(message);
+  
+  window.open(whatsappURL);
+
+  document.getElementById("backButton").style.display = "block";
+
+}//ini adalah baris terakhir dari link to what app
+
+
+// Ini adalah fitur order untuk card 3
+var modalketiga = document.getElementById("orderForm3");
+var spanketiga = document.getElementsByClassName("close close-3")[0]; // Gunakan indeks pertama
+var orderLinkketiga = document.getElementsByClassName("order-link-3");
+
+Array.from(orderLinkketiga).forEach(function(link) {
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    var coffeeNameketiga = this.getAttribute("data-name-3");
+    var priceketiga = this.getAttribute("data-price-3");
+
+    document.getElementById("coffeeName3").value = coffeeNameketiga; // Tetapkan nilai kopi
+    document.getElementById("price3").value = priceketiga; // Tetapkan harga kopi
+
+    modalketiga.style.display = "block";
+  });
+});
+
+
+spanketiga.onclick = function() {
+  modalketiga.style.display = "none";
+};
+
+window.onclick = function(event) {
+  if (event.target == modalketiga) {
+    modalketiga.style.display = "none";
+  }
+};
+
+document.getElementById("orderFormContent3").addEventListener("submit", function(event) {
+  event.preventDefault();
+  var coffeeNameketiga = document.getElementById("coffeeName3").value;
+  var priceketiga = document.getElementById("price3").value;
+  var quantityketiga = document.getElementById("quantity3").value;
+
+  modalketiga.style.display = "none";
+});
+
+
+//link to whats app card 3
+function orderNow3 (){
+  var coffeeNameketiga = document.getElementById("coffeeName3").value;
+  var priceketiga = document.getElementById("price3").value;
+  var quantityketiga = document.getElementById("quantity3").value;
+
+  var message = "Pesanan Anda : \n" + coffeeNameketiga + "\nHarga satuan : " + priceketiga + "\nHarga Total : " + (priceketiga * quantityketiga);
 
   var adminPhoneNumber = "6285788322061";
   var whatsappURL = "https://api.whatsapp.com/send?phone=" + adminPhoneNumber + "&text=" + encodeURIComponent(message);
